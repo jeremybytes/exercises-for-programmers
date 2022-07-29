36cf30beb2070f28e97d3b53efb49ec8dd05011d
Jeremy Clark <jeremy@jeremybytes.com>
2022-07-28 23:10:01 -0700

    Added csharp ex 02 (first step)

---
+++ csharp/02-counting-characters/v1/counting-test/CountingTests.cs
@@ -0,0 +1,51 @@
+using counting;
+
+namespace counting_test;
+
+public class Tests
+{
+    [Test]
+    public void GetCharacterCount_WhenHello_Returns5()
+    {
+        string input = "hello";
+        int expected = 5;
+
+        int count = Program.GetCharacterCount(input);
+
+        Assert.That(count, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void GetCharacterCount_WhenNull_Returns0()
+    {
+        string? input = null;
+        int expected = 0;
+
+        int count = Program.GetCharacterCount(input);
+
+        Assert.That(count, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void GetCharacterCount_WhenEmpty_Returns0()
+    {
+        string input = "";
+        int expected = 0;
+
+        int count = Program.GetCharacterCount(input);
+
+        Assert.That(count, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void GetOutputMessage_OnValid_ReturnsExpected()
+    {
+        string input = "hello";
+        int count = 5;
+        string expected = "hello has 5 characters.";
+
+        string message = Program.GetOutputMessage(input, count);
+
+        Assert.That(message, Is.EqualTo(expected));
+    }
+}
---
+++ csharp/02-counting-characters/v1/counting/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace counting;
+
+// Example
+// What is the input string? Homer
+// Homer has 5 characters.
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("What is the input string? ");
+        string? input = Console.ReadLine();
+        int count = GetCharacterCount(input);
+        string message = GetOutputMessage(input, count);
+        Console.WriteLine(message);
+    }
+
+    public static int GetCharacterCount(string? input)
+    {
+        if (input is null)
+            return 0;
+        return input.Length;
+    }
+
+    public static string GetOutputMessage(string? input, int count)
+    {
+        if (input is null)
+            input = "";
+        return $"{input} has {count} characters.";
+    }
+}
